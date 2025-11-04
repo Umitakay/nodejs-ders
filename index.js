955a6a8b8a04b4f62c8e5f61228277b0a51dd688
Erbil Ümit Akay <erbilakay@gmail.com>
2025-11-04 12:47:16 +0300

    -Middleware ekle -gitignore ekle

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require("./api/server.js");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = server;
